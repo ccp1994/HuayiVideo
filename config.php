0efa7781dab2021c82b352cd91def151a627c4f2
chenchunpeng1994 <chenchunpeng1994@163.com>
2019-06-27 15:45:28 +0800

    Initial commit

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: https://www.pineal.cn/
+// | @author_url 2: https://www.pineal.cn/
+// | @author_email: 18581281315@163.com   
+// +------------------------------------------------------------------------+
+// | ShongGuo - The Ultimate Video Sharing Platform
+// | Copyright (c) 2018 ShongGuo. All rights reserved.
+// +------------------------------------------------------------------------+
+// 数据库地址
+$sql_db_host = "localhost";
+// 数据库用户
+$sql_db_user = "huayi_school";
+// 数据库密码
+$sql_db_pass = "mtfSHMGCms8dBfM2";
+// 数据库名称
+$sql_db_name = "huayi_school";
+
+// 网站 URL
+$site_url = "http://hy.huayuexh.com"; // 比如 (https://www.pineal.cn)
+
+$purchase_code = "777777"; // 
+?>
